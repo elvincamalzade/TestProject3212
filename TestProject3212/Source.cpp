9faa2425161be711131f6d0cfe43737a7f5c3329
Ayxan Əhmədzadə <Ahme_if16@itstep.edu.az>
2022-03-06 17:29:28 +0400

    I add dosmth function

--- TestProject3212/Source.cpp
+++ TestProject3212/Source.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 
+void dosomething()
+{
+	cout << "I write awesome codes" << endl;
+}
+
 void main() {
 	cout << "Salam Github" << endl;
 	cout << "salam I Maa huseyn" << endl;
